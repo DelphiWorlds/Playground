00df4a2ac12bed9ddc292db3473e70047ca9455a
Dave Nottage <davidn@radsoft.com.au>
2022-01-03 15:31:18 +1030

    Added handling of alarms

--- Demos/Location/Service/LD.ServiceModule.pas
+++ Demos/Location/Service/LD.ServiceModule.pas
@@ -10,6 +10,8 @@ uses
 type
   TServiceModule = class(TAndroidIntentService)
     procedure AndroidIntentServiceHandleIntent(const Sender: TObject; const AnIntent: JIntent);
+  private
+    procedure HandleAlarm(const AIntent: JIntent);
   end;
 
 var
@@ -23,7 +25,8 @@ implementation
 
 uses
   Androidapi.Helpers, Androidapi.JNI.Location, Androidapi.JNI.JavaTypes,
-  DW.Androidapi.JNI.Location, DW.LocationHelpers.Android,
+  DW.OSLog,
+  DW.Androidapi.JNI.Location, DW.Androidapi.JNI.Os, DW.LocationHelpers.Android, DW.Consts.Android,
   LD.LocationUpdater;
 
 procedure TServiceModule.AndroidIntentServiceHandleIntent(const Sender: TObject; const AnIntent: JIntent);
@@ -41,7 +44,25 @@ begin
       LHelper.BroadcastLocationData(LLocation);
       TLocationUpdater.HandleLocationData(LHelper.Data);
     end;
+  end
+  else if AnIntent.getAction <> nil then
+  begin
+    if AnIntent.getAction.equals(StringToJString(cDWFusedLocationClientActionAlarm)) then
+      HandleAlarm(AnIntent);
   end;
+end;
+
+procedure TServiceModule.HandleAlarm(const AIntent: JIntent);
+var
+  LTimestamp, LInterval, LCurrentTimestamp: Int64;
+begin
+  TOSLog.d('Alarm intent: %s', [JStringToString(AIntent.toUri(0))]);
+  LInterval := AIntent.getLongExtra(StringToJString(cDWFusedLocationClientExtraAlarmInterval), 0);
+  LTimestamp := AIntent.getLongExtra(StringToJString(cDWFusedLocationClientExtraAlarmTimestamp), 0);
+  LCurrentTimestamp := TJSystem.JavaClass.currentTimeMillis;
+  // Here, the difference between LCurrentTimestamp and the sent LTimestamp could be checked to determine whether the alarm was fired while
+  //   the device was in doze mode. A choice then could be made to discard the intent.
+  // Otherwise, HandleAlarm could end up handling two consecutive intents
 end;
 
 end.
