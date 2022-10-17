7560a3e33c753ca4ce4e915772cca288971216cc
Dave Nottage <davidn@radsoft.com.au>
2022-10-18 07:29:57 +1030

    Various
    Renamed unit
    Added support for stream metadata

--- Demos/RadioPlayer/DW.RadioPlayer.ServiceController.pas
+++ Demos/RadioPlayer/DW.RadioPlayer.ServiceController.pas
@@ -1,4 +1,4 @@
-unit DW.RadioServiceController;
+unit DW.RadioPlayer.ServiceController;
 
 interface
 
@@ -16,42 +16,52 @@ uses
 
 type
   TRadioStatusChangedEvent = procedure(Sender: TObject; const Status: TRadioStatus) of object;
-  TStateEvent = procedure(Sender: TObject; const State: Integer) of object;
 
   TCustomRadioServiceController = class(TObject)
   private
     FServiceName: string;
     FOnRadioStatusChanged: TRadioStatusChangedEvent;
     FOnServiceStarted: TNotifyEvent;
+    FOnStreamMetadata: TStreamMetadataEvent;
   protected
     procedure DoRadioStatusChanged(const AStatus: TRadioStatus);
     procedure DoServiceStarted;
+    procedure DoStreamMetadata(const AMetadata: string);
   public
     procedure Pause; virtual;
     procedure Play; overload; virtual;
     procedure Play(const AURL: string); overload; virtual;
-    procedure StartService(const AServiceName: string); virtual;
+    // Returns True if the service is already running
+    function StartService(const AServiceName: string): Boolean; virtual;
     procedure Stop; virtual;
     property ServiceName: string read FServiceName write FServiceName;
     property OnRadioStatusChanged: TRadioStatusChangedEvent read FOnRadioStatusChanged write FOnRadioStatusChanged;
     property OnServiceStarted: TNotifyEvent read FOnServiceStarted write FOnServiceStarted;
+    property OnStreamMetadata: TStreamMetadataEvent read FOnStreamMetadata write FOnStreamMetadata;
   end;
 
 {$IF Defined(ANDROIDAPP)}
-  TServiceStateReceiver = class(TMultiReceiver)
+  TMessageEvent = procedure(Sender: TObject; const Msg: string) of object;
+  TStateEvent = procedure(Sender: TObject; const State: Integer) of object;
+
+  TServiceReceiver = class(TMultiReceiver)
   private
+    FOnMessage: TMessageEvent;
     FOnState: TStateEvent;
+    procedure DoMessage(const AMsg: string);
     procedure DoState(const AState: Integer);
   protected
     procedure Receive(context: JContext; intent: JIntent); override;
     procedure ConfigureActions; override;
   public
+    property OnMessage: TMessageEvent read FOnMessage write FOnMessage;
     property OnState: TStateEvent read FOnState write FOnState;
   end;
 
   TPlatformRadioServiceController = class(TCustomRadioServiceController)
   private
-    FReceiver: TServiceStateReceiver;
+    FReceiver: TServiceReceiver;
+    procedure ReceiverMessageHandler(Sender: TObject; const AMsg: string);
     procedure ReceiverStateHandler(Sender: TObject; const AState: Integer);
   public
     constructor Create;
@@ -59,7 +69,7 @@ type
     procedure Pause; override;
     procedure Play; overload; override;
     procedure Play(const AURL: string); overload; override;
-    procedure StartService(const AServiceName: string); override;
+    function StartService(const AServiceName: string): Boolean; override;
     procedure Stop; override;
   end;
 {$ENDIF}
@@ -67,9 +77,10 @@ type
 implementation
 
 uses
+  System.JSON,
   {$IF Defined(ANDROIDAPP)}
   Androidapi.Helpers, Androidapi.JNI.JavaTypes,
-  DW.ServiceCommander.Android,
+  DW.ServiceCommander.Android, DW.Android.Helpers,
   {$ENDIF}
   DW.Consts.Android;
 
@@ -87,6 +98,12 @@ begin
     FOnServiceStarted(Self);
 end;
 
+procedure TCustomRadioServiceController.DoStreamMetadata(const AMetadata: string);
+begin
+  if Assigned(FOnStreamMetadata) then
+    FOnStreamMetadata(Self, AMetadata);
+end;
+
 procedure TCustomRadioServiceController.Pause;
 begin
   //
@@ -102,9 +119,9 @@ begin
   //
 end;
 
-procedure TCustomRadioServiceController.StartService(const AServiceName: string);
+function TCustomRadioServiceController.StartService(const AServiceName: string): Boolean;
 begin
-  //
+  Result := True;
 end;
 
 procedure TCustomRadioServiceController.Stop;
@@ -113,23 +130,32 @@ begin
 end;
 
 {$IF Defined(ANDROIDAPP)}
-{ TServiceStateReceiver }
+{ TServiceReceiver }
 
-procedure TServiceStateReceiver.ConfigureActions;
+procedure TServiceReceiver.ConfigureActions;
 begin
   IntentFilter.addAction(StringToJString(cServiceStateAction));
+  IntentFilter.addAction(StringToJString(cServiceMessageAction));
 end;
 
-procedure TServiceStateReceiver.DoState(const AState: Integer);
+procedure TServiceReceiver.DoState(const AState: Integer);
 begin
   if Assigned(FOnState) then
     FOnState(Self, AState);
 end;
 
-procedure TServiceStateReceiver.Receive(context: JContext; intent: JIntent);
+procedure TServiceReceiver.Receive(context: JContext; intent: JIntent);
 begin
   if intent.getAction.equals(StringToJString(cServiceStateAction)) then
-    DoState(intent.getIntExtra(StringToJString(cServiceBroadcastParamState), cServiceStateUnknown));
+    DoState(intent.getIntExtra(StringToJString(cServiceBroadcastParamState), cServiceStateUnknown))
+  else if intent.getAction.equals(StringToJString(cServiceMessageAction)) then
+    DoMessage(JStringToString(intent.getStringExtra(StringToJString(cServiceBroadcastParamMessage))));
+end;
+
+procedure TServiceReceiver.DoMessage(const AMsg: string);
+begin
+  if Assigned(FOnMessage) then
+    FOnMessage(Self, AMsg);
 end;
 
 { TPlatformRadioServiceController }
@@ -137,7 +163,8 @@ end;
 constructor TPlatformRadioServiceController.Create;
 begin
   inherited;
-  FReceiver := TServiceStateReceiver.Create(True);
+  FReceiver := TServiceReceiver.Create(True);
+  FReceiver.OnMessage := ReceiverMessageHandler;
   FReceiver.OnState := ReceiverStateHandler;
 end;
 
@@ -145,6 +172,30 @@ destructor TPlatformRadioServiceController.Destroy;
 begin
   FReceiver.Free;
   inherited;
+end;
+
+procedure TPlatformRadioServiceController.ReceiverMessageHandler(Sender: TObject; const AMsg: string);
+var
+  LJSON: TJSONValue;
+  LMessageType: Integer;
+  LContent: string;
+begin
+  LJSON := TJSONObject.ParseJSONValue(AMsg);
+  if LJSON <> nil then
+  try
+    if LJSON.TryGetValue('MessageType', LMessageType) then
+    begin
+      case LMessageType of
+        cServiceMessageTypeRadioStreamMetadata:
+        begin
+          if LJSON.TryGetValue('Content', LContent) then
+            DoStreamMetadata(LContent);
+        end;
+      end;
+    end;
+  finally
+    LJSON.Free;
+  end;
 end;
 
 procedure TPlatformRadioServiceController.ReceiverStateHandler(Sender: TObject; const AState: Integer);
@@ -175,9 +226,11 @@ begin
   TServiceCommander.SendCommandJSON(TRadioCommand.Create(cServiceCommandRadioPlay, AURL).ToJSON);
 end;
 
-procedure TPlatformRadioServiceController.StartService(const AServiceName: string);
+function TPlatformRadioServiceController.StartService(const AServiceName: string): Boolean;
 begin
-  TServiceCommander.StartService(AServiceName);
+  Result := TAndroidHelperEx.IsServiceRunning(AServiceName);
+  if not Result then
+    TServiceCommander.StartService(AServiceName);
 end;
 
 procedure TPlatformRadioServiceController.Stop;
